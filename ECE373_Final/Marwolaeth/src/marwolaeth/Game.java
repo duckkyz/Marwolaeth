e27d03555f9294734fedf599ac14dc026ad8c067
duckkyz <duckkyz@ENGR-ECE232PC13.catnet.arizona.edu>
2016-11-04 12:50:18 -0700

    Collision works for multiple entitiy collision
    Still need to fix it to work with single entitiy collision but its
    beautiful and kind of simple

--- ECE373_Final/Marwolaeth/src/marwolaeth/Game.java
+++ ECE373_Final/Marwolaeth/src/marwolaeth/Game.java
@@ -26,7 +26,7 @@ public class Game {
 		}
 		
 		//Testing walls
-		for(int i=0;i<10;++i){
+		for(int i=5;i<10;++i){
 			drawables.add(new Wall((2*250)-64,(i*64)));
 			drawables.add(new Wall((i*64),(2*250)-64));
 		}
@@ -52,62 +52,6 @@ public class Game {
 		drawables.remove(drawable);
 	}
 	
-	public void stopUpMovement(){
-		if(hero.getDirection() == 0){
-			hero.setIsMoving(false);
-		}	
-		else if(hero.getDirection() == 45){
-			hero.setIsMoving(true);
-			hero.setDirection(90);
-		}
-		else if(hero.getDirection() == 315){
-			hero.setIsMoving(true);
-			hero.setDirection(270);
-		}
-	}
-
-	public void stopDownMovement(){
-		if(hero.getDirection() == 180){
-			hero.setIsMoving(false);
-		}	
-		else if(hero.getDirection() == 135){
-			hero.setIsMoving(true);
-			hero.setDirection(90);
-		}
-		else if(hero.getDirection() == 225){
-			hero.setIsMoving(true);
-			hero.setDirection(270);
-		}
-	}
-	
-	public void stopLeftMovement(){
-		if(hero.getDirection() == 270){
-			hero.setIsMoving(false);
-		}	
-		else if(hero.getDirection() == 315){
-			hero.setIsMoving(true);
-			hero.setDirection(0);
-		}
-		else if(hero.getDirection() == 225){
-			hero.setIsMoving(true);
-			hero.setDirection(180);
-		}
-	}
-	
-	public void stopRightMovement(){
-		if(hero.getDirection() == 90){
-			hero.setIsMoving(false);
-		}	
-		else if(hero.getDirection() == 45){
-			hero.setIsMoving(true);
-			hero.setDirection(0);
-		}
-		else if(hero.getDirection() == 135){
-			hero.setIsMoving(true);
-			hero.setDirection(180);
-		}
-	}
-	
 	public void checkForMapEdgeCollision(ArrayList<Drawable> drawables, Hero hero){
 		
 		
@@ -120,183 +64,69 @@ public class Game {
 		boolean canGoDown = true;
 		boolean canGoLeft = true;
 		boolean canGoRight = true;
-
+		
+		//TODO: fix this for single collision
 		System.out.println("New cycle");
-		for(int x=0;drawables.size()>x;x++){		
+		for(int x=0;drawables.size()>x;x++){
 			Drawable d = drawables.get(x);	
 			
 			if((newX >= d.getXPos()) & (newX <= (d.getXPos() + 64))){ 		//Collision from the left
 				if((newY >= d.getYPos()) & (newY <= (d.getYPos() + 64))){	//Collision from the bottom
 					System.out.println("Collision 1 for " + d.getXPos() + "," + d.getYPos());
-					canGoLeft = false;
-					canGoUp = false;
-					
-					/*if(hero.getDirection() == 270){
-						canGoLeft = false;
-						System.out.println("I'm trying to go left");
+					if(hero.getDirection() == 0){
+						canGoUp = false;
 					}
-					else if(hero.getDirection() == 315){
-						canGoLeft = false;
-						//hero.setDirection(0);
-						System.out.println("I'm trying to go up-left");
+					else if(hero.getDirection() == 45){
+						canGoUp = false;
 					}
 					else if(hero.getDirection() == 225){
 						canGoLeft = false;
-						//hero.setDirection(180);
-						System.out.println("I'm trying to go down-left");
-					}		
-					else if(hero.getDirection() == 0){
-						canGoUp = false;
 					}
-					*/
 				}
 				else if((newMaxY >= (d.getYPos())) & (newMaxY <= (d.getYPos() + 64))){	//Collision from top
 					System.out.println("Collision 2 for " + d.getXPos() + "," + d.getYPos());
-					canGoLeft = false;
-					canGoDown = false;
-					/*if(hero.getDirection() == 270){
+					if(hero.getDirection() == 135){
+						canGoDown = false;
+					}
+					else if(hero.getDirection() == 270){
 						canGoLeft = false;
-						System.out.println("I'm trying to go left");
 					}
 					else if(hero.getDirection() == 315){
 						canGoLeft = false;
-						//hero.setDirection(0);
-						System.out.println("I'm trying to go up-left");
-					}
-					else if(hero.getDirection() == 225){
-						canGoLeft = false;
-						//hero.setDirection(180);
-						System.out.println("I'm trying to go down-left");
-					}
-					*/		
+					}	
 				}
 			}
 			else if ((newMaxX >= d.getXPos()) & (newMaxX <= (d.getXPos() + 64))){	//Collision from the right
 				if((newY >= d.getYPos()) & (newY <= (d.getYPos() + 64))){
 					System.out.println("Collision 3 for " + d.getXPos() + "," + d.getYPos());
-					canGoRight = false;
-					canGoUp = false;
-					/*
-					if(hero.getDirection() == 90){
-						//hero.setIsMoving(false);
-						System.out.println("I'm trying to go right");
-					}
-					else if(hero.getDirection() == 45){
-						//hero.setDirection(0);
-						System.out.println("I'm trying to go up-right");
-					}
-					else if(hero.getDirection() == 135){
-						//hero.setDirection(180);
-						System.out.println("I'm trying to go down-right");
-					}		
-					else if(hero.getDirection() == 0){
+					if(hero.getDirection() == 0){
 						canGoUp = false;
 					}
-					*/
+					else if(hero.getDirection() == 90){
+						canGoRight = false;
+					}
+					else if(hero.getDirection() == 135){
+						canGoRight = false;
+					}
+					else if(hero.getDirection() == 315){
+						canGoUp = false;
+					}
 				}
 				else if((newMaxY >= (d.getYPos())) & (newMaxY <= (d.getYPos() + 64))){
 					System.out.println("Collision 4 for " + d.getXPos() + "," + d.getYPos());
-					canGoRight = false;
-					canGoDown = false;
-				}
-			}
-			//Collision types:
-			//top: newMinX < (d.getXPos() + 32)
-			//topLeft: (newMinX < (d.getXPos() + 32)) & (newMinY > 
-			//
-			//
-			
-			//TODO: Fix collision for non wall entities			
-			
-			/*
-			
-			
-			//Implements top collision
-			if(d.getYPos() == 0){
-				if((newY < (d.getYPos() + 64)) & (newY > d.getYPos())){
-					if((newX < d.getXPos()) & (newMaxX > d.getXPos())){
-						System.out.println("1. Wont move up because newY: " + newY + " <= d.getYPos + 64: " + (d.getYPos() + 64));
-						System.out.println();
-						stopUpMovement();
-					}	
-					else if((newX < (d.getXPos() + 64)) & (newMaxX > (d.getXPos() + 64))){
-						System.out.println("2. Wont move up because newY: " + newY + " <= d.getYPos + 64: " + (d.getYPos() + 64));
-						System.out.println();
-						stopUpMovement();
+					if(hero.getDirection() == 45){
+						canGoRight = false;
 					}
-					else if((newX == (d.getXPos())) & (newMaxX == (d.getXPos() + 64))){
-						System.out.println("3. Wont move up because newY: " + newY + " <= d.getYPos + 64: " + (d.getYPos() + 64));
-						System.out.println();
-						stopUpMovement();
+					else if(hero.getDirection() == 180){
+						canGoDown = false;
+					}
+					else if(hero.getDirection() == 225){
+						canGoDown = false;
 					}
 				}
 			}
-			
-			//Bot collision
-			if(d.getYPos() == ((1080 * 2) - 64)){
-				if((newMaxY > d.getYPos()) & (newY < d.getYPos())){
-					if((newX < d.getXPos()) & (newMaxX > d.getXPos())){
-						System.out.println("1. Wont move down because newMaxY: " + newMaxY + " >= d.getYPos: " + d.getYPos());
-						System.out.println();
-						stopDownMovement();
-					}	
-					else if((newX < (d.getXPos() + 64)) & (newMaxX > (d.getXPos() + 64))){
-						System.out.println("2. Wont move down because newMaxY: " + newMaxY + " >= d.getYPos: " + d.getYPos());
-						System.out.println();
-						stopDownMovement();
-					}
-					else if((newX == (d.getXPos())) & (newMaxX == (d.getXPos() + 64))){
-						System.out.println("3. Wont move down because newMaxY: " + newMaxY + " >= d.getYPos: " + d.getYPos());
-						System.out.println();
-						stopDownMovement();
-					}
-				}
-			}
-			
-			if(d.getXPos() == ((1920 * 2) - 64)){
-				//Right Collision
-				if((newMaxX > d.getXPos()) & (newX < d.getXPos())){
-					if((newY < d.getYPos()) & (newMaxY > d.getYPos())){
-						System.out.println("1. Wont move right because newMaxX: " + newX + " >= d.getXPos: " + d.getXPos());
-						System.out.println();
-						stopRightMovement();
-					}	
-					else if((newY < (d.getYPos() + 64)) & (newMaxY > (d.getYPos() + 64))){
-						System.out.println("2. Wont move right because newMaxX: " + newX + " >= d.getXPos: " + d.getXPos());
-						System.out.println();
-						stopRightMovement();
-					}
-					else if((newY == (d.getYPos())) & (newMaxY == (d.getYPos() + 64))){
-						System.out.println("3. Wont move right because newMaxX: " + newX + " >= d.getXPos: " + d.getXPos());
-						System.out.println();
-						stopRightMovement();
-					}
-				}
-			}
-			
-			if(d.getXPos() == 0){
-				//Left Collision
-				if((newX < (d.getXPos() + 64)) & (newMaxX > (d.getXPos() + 64))){
-					if((newY < d.getYPos()) & (newMaxY > d.getYPos())){
-						System.out.println("1. Wont move left because newX: " + newX + " <= d.getXPos + 64: " + (d.getXPos() + 64));
-						System.out.println();
-						stopLeftMovement();
-					}	
-					else if((newY < (d.getYPos() + 64)) & (newMaxY > (d.getYPos() + 64))){
-						System.out.println("2. Wont move left because newX: " + newX + " <= d.getXPos + 64: " + (d.getXPos() + 64));
-						System.out.println();
-						stopLeftMovement();
-					}
-					else if((newY == (d.getYPos())) & (newMaxY == (d.getYPos() + 64))){
-						System.out.println("3. Wont move left because newX: " + newX + " <= d.getXPos + 64: " + (d.getXPos() + 64));
-						System.out.println();
-						stopLeftMovement();
-					}
-				}			
-			}
-			*/
 		}
-
+		//TODO: take this into seperate method
 		if(hero.getDirection() == 0){
 			if(canGoUp == false){
 				hero.setIsMoving(false);
@@ -368,6 +198,7 @@ public class Game {
 				hero.setDirection(0);
 			}
 		}
+		
 	}
 	
 	public void doGameLogic(Set keySet) {
