53735ac34fd9d4af29ec5b11e3ebc4b928584a66
Ben <Ben@dhcp-10-132-170-163.uawifi.arizona.edu>
2016-11-15 08:57:50 -0700

    Added a leaf for my druid

---
+++ ECE373_Final/Marwolaeth/src/marwolaeth/ImplementedProjectiles/Leaf.java
@@ -0,0 +1,71 @@
+package marwolaeth.ImplementedProjectiles;
+
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+import marwolaeth.DrawableClasses.Projectile;
+import marwolaeth.DrawableClasses.Sprite;
+
+public class Leaf extends Projectile {
+	
+	public Leaf(int direction, int spawnX, int spawnY, boolean heroProjectile) {
+		super(direction, spawnX, spawnY, heroProjectile);
+		try{
+			setGraphic(rotate((ImageIO.read(new File("Drawable_Images/Leaves.png"))), 90));
+		}
+		catch(IOException ex){
+			
+		}
+		
+		setActionSequence(0);
+		setActionStep(0);
+		setTileWidth(getGraphic().getWidth());
+		setTileHeight(getGraphic().getHeight());
+		setIsMoving(true);
+		
+		setXPos(getXPos()-getTileWidth()/2);
+		setYPos(getYPos()-getTileHeight()/2);
+		
+		if(direction >= 0 & direction <= 90) {
+			setTopHitBox(1);		
+			setBotHitBox(1+32*(int)Math.abs(Math.cos(Math.toRadians(direction))));		
+			setLeftHitBox(1+32*(int)Math.abs(Math.sin(Math.toRadians(direction))));		
+			setRightHitBox(1);		
+		}
+		else if(direction >= 90 & direction <= 180) {
+			setTopHitBox(1+32*(int)Math.abs(Math.cos(Math.toRadians(direction))));		
+			setBotHitBox(1);		
+			setLeftHitBox(1+32*(int)Math.abs(Math.sin(Math.toRadians(direction))));	
+			setRightHitBox(1);		
+		}
+		else if(direction >= 180 & direction <= 270) {
+			setTopHitBox(1+32*(int)Math.abs(Math.cos(Math.toRadians(direction))));		
+			setBotHitBox(1);		
+			setLeftHitBox(1);		
+			setRightHitBox(1+32*(int)Math.abs(Math.sin(Math.toRadians(direction))));	
+		}
+		else if(direction >= 270 & direction <= 360) {
+			setTopHitBox(0);		
+			setBotHitBox(1+32*(int)Math.abs(Math.cos(Math.toRadians(direction))));		
+			setLeftHitBox(0);	
+			setRightHitBox(1+32*(int)Math.abs(Math.sin(Math.toRadians(direction))));	
+		}
+		
+		setSpeed(20);
+		setDamage(25);
+	}
+
+	public void doLogic() {
+
+	}
+	
+	public void attack(Sprite beingAttacked){
+		int newHealth = beingAttacked.getHealth() - getDamage();
+		if(newHealth < 0){
+			System.out.println(beingAttacked.getClass().getSimpleName() + " died to a leaf! lol, nerd.");
+		}
+		beingAttacked.setHealth(newHealth);
+	}
+}
