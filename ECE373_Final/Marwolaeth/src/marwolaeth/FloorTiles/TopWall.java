1cf0245942fa88f3d273d37a8d343120f89bad6b
Ben <Ben@10.181.193.243>
2016-11-20 22:31:45 -0700

    Added cosmetic walls
    1. Going to add in other scenary things later

--- ECE373_Final/Marwolaeth/src/marwolaeth/FloorTiles/TopWall.java
+++ ECE373_Final/Marwolaeth/src/marwolaeth/FloorTiles/TopWall.java
@@ -8,8 +8,14 @@ public class TopWall extends Wall {
 	public TopWall(int x, int y){
 		super(x,y);
 		
-		setActionStep((int) (6 + Math.floor(Math.random() * 0)));		//column
-		setActionSequence((int) (0 + Math.floor(Math.random() * 0)));	//row
+		if((Math.random() * 100) > 75){
+			setActionStep((int) (0 + Math.floor(Math.random() * 4)));		//column
+			setActionSequence((int) (3 + Math.floor(Math.random() * 0)));	//row
+		}
+		else{
+			setActionStep((int) (6 + Math.floor(Math.random() * 0)));		//column
+			setActionSequence((int) (0 + Math.floor(Math.random() * 0)));	//row
+		}
 		this.setTileHeight(128);
 		setBotHitBox(8);
 	}
